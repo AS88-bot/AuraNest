8fd597bfa2d8fb8ebc92e84de191bc67e5804cbc
Aisha S <aishaforlife29@gmail.com>
2025-11-01 17:31:02 +0000

    Error: A tree hydrated but some attributes of the server rendered HTML d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,11 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" key="body">
+      <body className="font-body antialiased">
         <FirebaseClientProvider>
           <ClientLayout>{children}</ClientLayout>
         </FirebaseClientProvider>
-        <Toaster />
       </body>
     </html>
   );
 }
-
-    
--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { AppLayout } from '@/components/layout/app-layout';
+import { Toaster } from '@/components/ui/toaster';
 
 export default function ClientLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <AppLayout>{children}</AppLayout>;
+  return (
+    <AppLayout>
+      {children}
+      <Toaster />
+    </AppLayout>
+  );
 }
