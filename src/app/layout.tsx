81f79479a17d26b2d7d0a031003c5158eef9ae55
Aisha S <aishaforlife29@gmail.com>
2025-11-01 17:36:04 +0000

    it hasnt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
 import ClientLayout from '@/components/layout/client-layout';
 import { FirebaseClientProvider } from '@/firebase';
 
@@ -21,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased" suppressHydrationWarning>
         <FirebaseClientProvider>
           <ClientLayout>{children}</ClientLayout>
         </FirebaseClientProvider>
